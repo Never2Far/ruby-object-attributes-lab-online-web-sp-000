b0242876c5f55eeea9a2a2a74dbcc6ae76fa32d4
Seth Near <seth.j.near@gmail.com>
2020-06-21 03:17:12 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,9 +1,9 @@
 class Dog
 
-def initialize(dog_name, breed)
- name = dog_name
- breed = breed
-end
+# def initialize(dog_name, breed)
+#  name = dog_name
+#  breed = breed
+# end
 
 def name=(dog_name)
 @name = dog_name
